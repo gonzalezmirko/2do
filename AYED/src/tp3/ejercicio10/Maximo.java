e524df6f53d81221a595aac0c261023bc0658d49
Mirko Emiliano Gonzalez <mir_elmejor99@hotmail.com>
2025-04-29 22:44:56 -0300

    Subo ejercicio 10 sin probarlo

---
+++ AYED/src/tp3/ejercicio10/Maximo.java
@@ -0,0 +1,14 @@
+package tp3.ejercicio10;
+
+public class Maximo {
+
+	private int max=Integer.MIN_VALUE;
+	
+	public void setMax(int max) {
+		this.max = max;
+	}
+
+	public int getMax() {
+		return max;
+	}
+}
