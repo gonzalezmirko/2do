ac831be767f5db316973ed8b4a3afb310c372373
AnaGDP <anaguadalupedepasqua@hotmail.com>
2025-09-29 22:24:53 -0300

    Subo ejercicio 5 y 6

---
+++ ejercicio6/src/main/java/ar/edu/unlp/info/oo1/ejercicio6/CuadroTarifario.java
@@ -0,0 +1,20 @@
+package ar.edu.unlp.info.oo1.ejercicio6;
+
+public class CuadroTarifario {
+	private double costo;
+
+	public CuadroTarifario(double costo) {
+		super();
+		this.costo = costo;
+	}
+
+	public double getCosto() {
+		return costo;
+	}
+
+	public void setCosto(double costo) {
+		this.costo = costo;
+	}
+	
+	
+}
